fa3dff73a02b84c809c244acac0a6f665ad3890a
csjkevin <csjkevin@outlook.com>
2021-01-22 13:48:15 +0800

    add post json data in ajax

--- ajax.js
+++ ajax.js
@@ -1,18 +1,33 @@
-let xhr = new XMLHttpRequest();
-xhr.open('GET','api/test','true');
-xhr.send()
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-    	console.log(xhr.responseText);
-    }
-}
+// get something
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "/api/test", "true");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(xhr.responseText);
+  }
+};
+xhr.send();
 
-let xhr2 = new XMLHttpRequest();
-xhr2.open('POST','api/test','true');
-xhr2.setRequestHeader("content-type","application/x-www-form-urlencoded");
-xhr2.send("firstName=Kevin&lastName=Chen");
-xhr2.onreadystatechange = function() {
-    if (xhr2.readyState === 4 && xhr2.status === 200) {
-    	console.log(xhr2.responseText);
-    }
-}
+// post form data
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "/api/test", "true");
+xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(xhr.responseText);
+  }
+};
+xhr.send("firstName=Kevin&lastName=Chen");
+
+// post json data
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "/api/test", true);
+xhr.setRequestHeader("content-type", "application/json");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    console.log(xhr2.responseText);
+  }
+};
+let jsonData = { firstName: "Kevin", lastName: "Chen" };
+//将用户输入值序列化成字符串
+xhr.send(JSON.stringify(sendData));
