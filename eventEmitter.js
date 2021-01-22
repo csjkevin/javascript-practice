d774730269ede2c9f8398c88dbd23d5904f83c70
csjkevin <csjkevin@outlook.com>
2021-01-22 13:48:26 +0800

    add eventEmitter

---
+++ eventEmitter.js
@@ -0,0 +1,39 @@
+class EventEmitter {
+  constructor() {
+    this.subs = {};
+  }
+
+  on(eventName, callback) {
+    if (this.subs.hasOwnProperty(eventName)) {
+      this.subs[type] = [];
+    } else {
+      this.subs[type].push(callback);
+    }
+  }
+
+  emit(eventName, ...args) {
+    if (this.subs.hasOwnProperty(eventName)) {
+      if (this.subs.length > 0) {
+        this.subs[eventName].forEach((el) => {
+          el(...args);
+        });
+      }
+    }
+  }
+
+  off(eventName, callback) {
+    let targetIndex = this.subs[eventName].indexOf(callback);
+    if (targetIndex !== -1) {
+      this.subs[eventName].splice(targetIndex, 1);
+    }
+    if (this.subs[eventName].length === 0) {
+      delete this.subs[eventName];
+    }
+  }
+
+  offAll(eventName) {
+    if (this.subs.hasOwnProperty(eventName)) {
+      delete this.subs[eventName];
+    }
+  }
+}
