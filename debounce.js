6db8e664a23b8d7358fcf4da03a3213b2bb2166a
csjkevin <csjkevin@outlook.com>
2021-03-10 15:42:16 +0800

    reformat

--- debounce.js
+++ debounce.js
@@ -1,9 +1,9 @@
 function debounce(fn, interval) {
-	let timer = null;
-	return function() {
-		if (timer !== null) {
-			clearTimeout(timer);
-		}
-		timer = setTimeout(fn, interval);
-	}
+  let timer = null;
+  return function () {
+    if (timer !== null) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(fn, interval, ...arguments);
+  };
 }
